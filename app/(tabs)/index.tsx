6791a78ce6de593bfc1a3f9bd358410bc4bdaf10
ISABELLY RIBEIRO DOS SANTOS <isabelly.santos22@portalsesisp.org.br>
2026-05-19 15:02:22 -0300

    primeiro commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,80 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+// Importação de bibliotecas e componentes necessários
+import { useState } from 'react'
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+// Importação de componentes do React Native para construção da interface
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native'
 
-export default function HomeScreen() {
+// Componente principal do aplicativo
+export default function App() {
+
+  // Estados para armazenar os valores do álcool, gasolina e resultado
+  const [alcool, setAlcool] = useState('')
+  const [gasolina, setGasolina] = useState('')
+  const [resultado, setResultado] = useState('')
+
+  // Função para calcular qual combustível é mais vantajoso
+  function calcularCombustivel() {
+
+    const valorAlcool = Number(alcool)
+    const valorGasolina = Number(gasolina)
+
+    const calculo = valorAlcool / valorGasolina
+
+    if (calculo < 0.7) {
+      setResultado('Vale mais a pena usar Álcool')
+    } else {
+      setResultado('Vale mais a pena usar Gasolina')
+    }
+
+  }
+
+  // Renderização da interface do aplicativo
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+    <View>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <Text>
+        Calculadora de Combustível
+      </Text>
+
+      <Text style={{ marginBottom: 10 }}>
+         Insira os preços do Álcool
+      </Text>
+
+      <TextInput
+        placeholder="Preço do Álcool"
+        value={alcool}
+        //Atualiza o estado do álcool conforme o usuário digita
+        onChangeText={setAlcool}
+        keyboardType="numeric"
+      />
+
+      <Text>
+         Insira os preços do Gasolina
+      </Text>
+
+      <TextInput
+        placeholder="Preço da Gasolina"
+        value={gasolina}
+        //Atualiza o estado da gasolina conforme o usuário digita  
+        onChangeText={setGasolina}
+        keyboardType="numeric"
+      />
+
+      <Button
+        title="Calcular"
+        onPress={calcularCombustivel}
+      />
+
+      <Text>
+        {resultado}
+      </Text>
+
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
