cd4f949c97c6fcce9941c7aa58d4d076ee48f3b3
ISABELLY RIBEIRO DOS SANTOS <isabelly.santos22@portalsesisp.org.br>
2026-05-26 09:41:07 -0300

    primeiro commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -35,18 +35,19 @@ export default function App() {
   }
 
   // Renderização da interface do aplicativo
-  return (
-    <View>
+    return (
+    <View style={styles.container}>
 
-      <Text>
+      <Text style={styles.titulo}>
         Calculadora de Combustível
       </Text>
 
-      <Text style={{ marginBottom: 10 }}>
+      <Text style={styles.label}>
          Insira os preços do Álcool
       </Text>
 
       <TextInput
+        style={styles.input}
         placeholder="Preço do Álcool"
         value={alcool}
         //Atualiza o estado do álcool conforme o usuário digita
@@ -54,11 +55,12 @@ export default function App() {
         keyboardType="numeric"
       />
 
-      <Text>
+      <Text style={styles.label}>
          Insira os preços do Gasolina
       </Text>
 
       <TextInput
+        style={styles.input}
         placeholder="Preço da Gasolina"
         value={gasolina}
         //Atualiza o estado da gasolina conforme o usuário digita  
@@ -66,15 +68,52 @@ export default function App() {
         keyboardType="numeric"
       />
 
-      <Button
+      <Button 
+
         title="Calcular"
         onPress={calcularCombustivel}
       />
 
-      <Text>
+      <Text style={styles.resultado}>
         {resultado}
       </Text>
 
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+     flex:1,
+     justifyContent:'center',
+     padding:20,
+     backgroundColor:'#FFFFFF'
+  },
+
+ titulo: {
+    fontSize: 24,
+    fontWeight:'bold',
+    marginBottom: 20, 
+    textAlign: 'center'
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 15,
+    borderRadius: 8
+  },
+
+  label: {
+    marginBottom: 10,
+    fontSize: 16
+  },
+
+  resultado: {
+    marginTop: 20,
+    fontSize: 18,
+    textAlign: 'center'
+  }
+
+})
